f7d701593d7d5dc3104abd182094964278e33836
angga9912 <135648080+angga9912@users.noreply.github.com>
2026-09-21 15:06:14 +0700

    Add files via upload

--- app/src/main/java/com/nada/kasir/NadaKasirApp.kt
+++ app/src/main/java/com/nada/kasir/NadaKasirApp.kt
@@ -1,6 +1,7 @@
 package com.nada.kasir
 
 import android.app.Application
+import com.nada.kasir.core.util.BeepPlayer
 import com.nada.kasir.core.util.CrashReporter
 import dagger.hilt.android.HiltAndroidApp
 
@@ -9,5 +10,6 @@ class NadaKasirApp : Application() {
     override fun onCreate() {
         super.onCreate()
         CrashReporter.pasang(this)
+        BeepPlayer.init(this)
     }
 }
